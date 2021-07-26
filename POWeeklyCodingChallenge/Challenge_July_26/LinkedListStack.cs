17e5e2cbe5ab8add5e9442ea4ecc66e3d1558314
Robert Fisher <RobertNFisherJR@gmail.com>
2021-07-26 14:30:09 -0400

    Added a pointless LinkedListStack

---
+++ POWeeklyCodingChallenge/Challenge_July_26/LinkedListStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_July_26
+{
+    public class LinkedListStack
+    {
+        public LinkedList<int> myList { get; set; }
+        public LinkedListStack()
+        {
+            this.myList = new LinkedList<int>();
+        }
+        public int Pop()
+        {
+            int firstElement = myList.First();
+            myList.RemoveFirst();
+            return firstElement;
+        }
+        public void Push(int element)
+        {
+            myList.AddFirst(element);
+        }
+        public int Peek()
+        {
+            return myList.First();
+        }
+        public void print()
+        {
+            Utility.Printer.printIntArrayAsArray(myList.ToList());
+        }
+    }
+}
--- POWeeklyCodingChallenge/RunProjects/Program.cs
+++ POWeeklyCodingChallenge/RunProjects/Program.cs
@@ -8,14 +8,27 @@ namespace RunProjects
     {
         static void Main(string[] args)
         {
+            // Solves the Array Permutations problem from Week July 19 2021 Revature Problem
             int[] given = { 10, 20, 30 };
             int[] fourCase = { 1, 2, 3, 4 };
             //ArrayPermutation.Program s = new ArrayPermutation.Program();
+            
+            // Solves the Contiguous Array equal to sum problem from July 26 2021 Revature Problem
             int[] arr = { 14, 12, 70, 15, 99, 65, 21, 90 };
             int x = 97;
             J26.ContiguousArray CA = new J26.ContiguousArray();
-            CA.Solve(arr, x);
-            
+            //CA.Solve(arr, x);
+
+            J26.LinkedListStack stack = new J26.LinkedListStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            int top = stack.Peek();
+            Console.WriteLine(top);
+            stack.print();
+
+            stack.Pop();
+            stack.print();
         }
     }
 }
